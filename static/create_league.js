a1684fb3d762d4c3827a182e063081c99ca3fa48
mdlcodes <michelle.digay.landicho@gmail.com>
2026-07-14 21:42:36 +0800

    Feat: Synchronize team inputs to allow dynamic team selection for player section

--- static/create_league.js
+++ static/create_league.js
@@ -3,20 +3,55 @@ function toggleSection(sectionId){
     section.classList.toggle('active');
 }
 
+function updateTeamDropdowns(){
+    console.log("The function is running!");
+    const teamInputs = document.querySelectorAll('input[name="team_name[]"]');
+    const currentTeams = [];
+
+    teamInputs.forEach(input => {
+        if (input && input.value && input.value.trim() !== ''){
+            currentTeams.push(input.value.trim())
+        }
+    });
+
+    const dropdowns = document.querySelectorAll('select[name="player_team[]"]');
+
+    dropdowns.forEach(select => {
+        const previousSelection = select.value || "";
+
+        select.innerHTML = '<option value="">Select team</option>';
+
+        currentTeams.forEach(teamName => {
+            const option = document.createElement('option');
+            option.value = teamName;
+            option.textContent = teamName;
+
+            if(teamName === previousSelection){
+                option.selected = true;
+            }
+
+            select.appendChild(option);
+        });
+    });
+
+}
+
 function addTeamRow(){
     const wrapper = document.getElementById('teams-container-wrapper');
     const newRow = document.createElement('div');
     newRow.className = 'team-builder-row';
+    
     newRow.innerHTML = `
         <div class="sub-input-row">
-            <input type="text" name="team_name[]" placeholder="Enter team name" required>
+            <input type="text" name="team_name[]" placeholder="Enter team name" oninput="updateTeamDropdowns()" required>
             <input type="text" name="team_coach[]" placeholder="Enter coach name" required>
             <input type="text" name="team_colors[]" placeholder="Enter team colors" required>
-            <button type="button" onclick="this.parentElement.parentElement.remove()" style="background-color: #ef4444; color: white;">-</button>
+            <button type="button" onclick="this.parentElement.parentElement.remove(); updateTeamDropdowns();" style="background-color: #ef4444; color: white;">-</button>
         </div>
     `;
 
     wrapper.appendChild(newRow);
+    updateTeamDropdowns();
                             
 }
 
@@ -37,4 +72,5 @@ function addPlayerRow() {
         </div>
     `;
     wrapper.appendChild(newRow);
+    updateTeamDropdowns();
 }
